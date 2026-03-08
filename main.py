5778cbab8cf63b94663e43802e656245c154108e
stanleylew5 <stanley43595@gmail.com>
2026-03-07 17:33:16 -0800

    change file names again

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ def main():
     print("Welcome to Stanley's Search Algorithm!")
     print("Type in the name of the file to test : ")
     fileName = input()
-    
+    print("Type in the number of the algorithm you want to run.\n")
+    print("\t1. Forward Selection")
+    print("\t2. Backward Elimination")
+    algorithm = input()
 
 if __name__ == "__main__":
     main()
