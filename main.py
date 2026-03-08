8205205438029c33daba13d65b4017d34de3cf5b
stanleylew5 <stanley43595@gmail.com>
2026-03-07 17:38:28 -0800

    # of instances & features funcs

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+def findNumberOfFeatures(fileName: str) -> int:
+    with open(fileName, 'r') as file:
+        firstLine = file.readline()
+        features = firstLine.split()
+        return len(features) - 1
+
+def findNumberOfInstances(fileName: str) -> int:
+    with open(fileName, 'r') as file:
+        lines = file.readlines()
+        return len(lines)
+
 def main():
     print("Welcome to Stanley's Search Algorithm!")
     print("Type in the name of the file to test : ")
@@ -6,6 +17,9 @@ def main():
     print("\t1. Forward Selection")
     print("\t2. Backward Elimination")
     algorithm = input()
+    numFeatures = findNumberOfFeatures(fileName)
+    numInstances = findNumberOfInstances(fileName)
+    print("This dataset has " + str(numFeatures) + " features (not including the class attribute), with " + str(numInstances) + " instances.")
 
 if __name__ == "__main__":
     main()
