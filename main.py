2f608cd6db5166b8db927110b30c83613739468e
stanleylew5 <stanley43595@gmail.com>
2026-03-07 23:59:01 -0800

    euclidean distance & nearest neighbor with all features

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
+import math
+
 def organizeData(fileName: str) -> tuple[list[list[float]], list[int]]:
     data: list[list[float]] = []
-    labels: list[int] = []
+    classes: list[int] = []
     with open(fileName, "r") as f:
         for line in f:
             values: list[str] = line.split()
             if not values: continue
-            labels.append(int(float(values[0])))
+            classes.append(int(float(values[0])))
             data.append([float(x) for x in values[1:]])
-    return data, labels
+    return data, classes
 
+def euclideanDistance(x: list[float], y: list[float], featureIndices: set[int]) -> float:
+    return math.sqrt(sum([(x[i] - y[i]) ** 2 for i in featureIndices]))
+
+def nearestNeighbor(data: list[list[float]], classes: list[int], featureIndices: set[int]) -> float:
+    n: int = len(data) # of instances
+    correct: int = 0
+    for i in range(n):
+        nearestNeighborDistance: float = float('inf')
+        nearestNeighborClass: int = -1
+        for j in range(n):
+            if i == j: continue
+            distance: float = euclideanDistance(data[i], data[j], featureIndices)
+            if distance < nearestNeighborDistance:
+                nearestNeighborDistance = distance
+                nearestNeighborClass = classes[j]
+        if nearestNeighborClass == classes[i]: correct += 1
+    return correct / n
 
 def main():
     print("Welcome to Stanley's Search Algorithm!\n")
@@ -17,14 +36,16 @@ def main():
     algorithm = input().strip()
 
     data: list[list[float]]
-    labels: list[int]
-    data, labels = organizeData(fileName)
+    classes: list[int]
+    data, classes = organizeData(fileName)
 
     numFeatures: int = len(data[0])
     numInstances: int = len(data)
 
+    nearestNeighborAccuracy: float = nearestNeighbor(data, classes, set(range(numFeatures)))
+
     print(f"This dataset has {numFeatures} features (not including the class attribute), with {numInstances} instances.\n")
-    print(f"Running nearest neighbor with all {numFeatures} features, using \"leave-one-out\" evaluation, I get an accuracy of ")
+    print(f"Running nearest neighbor with all {numFeatures} features, using \"leave-one-out\" evaluation, I get an accuracy of {nearestNeighborAccuracy * 100:.2f}%")
 
 
 if __name__ == "__main__":
