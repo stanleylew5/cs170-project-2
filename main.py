de6957c1470562a59bed56ff7abe288ddde0dcc1
stanleylew5 <stanley43595@gmail.com>
2026-03-07 17:30:06 -0800

    keep only the datasets i need

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Welcome to Stanley's Search Algorithm!")
+def main():
+    print("Welcome to Stanley's Search Algorithm!")
+    print("Type in the name of the file to test : ")
+    fileName = input()
+    
+
+if __name__ == "__main__":
+    main()
